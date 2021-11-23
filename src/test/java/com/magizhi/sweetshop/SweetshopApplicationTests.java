d30dfa016a68e4392518a3987bac654973f128a5
raja351634 <rajaram.accet@gmail.com>
2021-11-23 17:27:08 -0500

    InitialVersion

---
+++ src/test/java/com/magizhi/sweetshop/SweetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magizhi.sweetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
